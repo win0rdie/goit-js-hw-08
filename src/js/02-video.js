6be35cbfea0f8c615227571859f38f4aa38e247e
win0rdie <rostyslavhnatovskyi@gmail.com>
2022-11-05 00:57:09 +0200

    change deploy and begin task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+// console.log(Player);
 
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+const player = new Player(iframe);
+console.log(player);
+
+const localeStorage = 0;
+player.on('timeupdate', function (e) {
+  console.log(e);
+  // console.log('videoplayer-current-time');
+});
+
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
