497e976c77e9619cb6facb071829ff61dc89c89c
win0rdie <rostyslavhnatovskyi@gmail.com>
2022-11-07 22:24:04 +0200

    8-1,8-2,8-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const feedbackForm = document.querySelector('.feedback-form');
+
+const formMessage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+function handleInput({ target }) {
+  formMessage[target.name] = target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formMessage));
+}
+
+function getDataFromLocalStotage() {
+  const data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+
+  if (data) {
+    email.value = data.email || '';
+    message.value = data.message || '';
+  }
+}
+
+getDataFromLocalStotage();
+
+function handleSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+feedbackForm.addEventListener('input', throttle(handleInput, 500));
+feedbackForm.addEventListener('submit', handleSubmitForm);
